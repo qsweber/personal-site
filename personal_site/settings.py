893505ea943303b3b9a200e596dc4c30f7779f54
Quinn Weber <quinn@quinnweber.com>
2017-09-02 10:57:32 -0700

    adding logging to settings

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -94,3 +94,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'personal_site.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
