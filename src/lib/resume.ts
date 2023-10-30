6c508d7887b1a6b8a0286f2597d50c12e70c9f20
Quinn Weber <quinn@quinnweber.com>
2023-10-30 09:12:40 -0700

    changes

--- src/lib/resume.ts
+++ src/lib/resume.ts
@@ -2,34 +2,38 @@ export interface Job {
   dateRange: string;
   title: string;
   company: string;
+  location: string;
   accomplishments: string[];
 }
 
 export const RESUME: Job[] = [
   {
-    dateRange: "2019 - Present",
-    title: "Software Engineer",
-    company: "Google",
+    dateRange: "2017 - Present",
+    location: "Seattle, WA",
+    title: "Senior Software Engineer",
+    company: "Convoy",
     accomplishments: [
+      "Build a platform to automatically ingest shipments via EDI that grew from 300 -> 8000 shipments/week",
       "Built a thing",
-      "Built another thing",
-      "Built a third thing",
-    ],
-  },
-  {
-    dateRange: "2017 - 2019",
-    title: "Software Engineer",
-    company: "Facebook",
-    accomplishments: [
-      "Built a thing",
-      "Built another thing",
       "Built a third thing",
     ],
   },
   {
     dateRange: "2015 - 2017",
+    location: "Seattle, WA",
     title: "Software Engineer",
-    company: "Microsoft",
+    company: "EnergySavvy",
+    accomplishments: [
+      "Lead a new engineering initiative responsible for building the ETL infrastructure that powers our analytics products",
+      "Built another thing",
+      "Built a third thing",
+    ],
+  },
+  {
+    dateRange: "2012 - 2015",
+    location: "Tyson's Corner, VA",
+    title: "Investment Analyst",
+    company: "DC Energy",
     accomplishments: [
       "Built a thing",
       "Built another thing",
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,24 +1,81 @@
-import { FunctionComponent } from "react";
-import { Job, RESUME } from "../lib/resume";
+import { RESUME } from "../lib/resume";
+import styled from "@emotion/styled";
 
-export const Jobb: FunctionComponent<{ job: Job }> = ({ job }) => (
-  <div>
-    <h3>{job.company}</h3>
-    <p>{job.title}</p>
-    <p>{job.dateRange}</p>
-    <ul>
-      {job.accomplishments.map((accomplishment) => (
-        <li>{accomplishment}</li>
-      ))}
-    </ul>
-  </div>
-);
+const SINGLE_SPACE = 4;
+
+const Container = styled.div(() => ({
+  paddingTop: SINGLE_SPACE * 4,
+  maxWidth: 670,
+  margin: "0 auto",
+  fontFamily: "Work Sans",
+}));
+
+const Line = styled.div(() => ({
+  display: "flex",
+  justifyContent: "space-between",
+  marginBottom: SINGLE_SPACE,
+}));
+
+const Company = styled.div(() => ({
+  fontWeight: "bold",
+  fontSize: "1.4em",
+}));
+
+const Location = styled.div(() => ({}));
+
+const DateRange = styled.div(() => ({
+  fontStyle: "italic",
+}));
+
+const Title = styled.div(() => ({
+  fontStyle: "italic",
+}));
+
+const Accomplishments = styled.ul(() => ({
+  listStylePosition: "outside",
+  paddingLeft: 16,
+  marginTop: 0,
+  marginBottom: 0,
+}));
+
+const Accomplishment = styled.li(() => ({}));
+
+const BottomWrapper = styled.div(() => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "flex-end",
+}));
 
 export const Resume = () => (
-  <div>
-    <h2>Resume</h2>
-    {RESUME.map((job) => (
-      <Jobb job={job} />
+  <Container>
+    <Line style={{ justifyContent: "center" }}>
+      <Company>Quinn Weber</Company>
+    </Line>
+    <Line style={{ justifyContent: "center" }}>
+      Seattle, WA - quinn@quinnweber.com
+    </Line>
+    {RESUME.map((job, index) => (
+      <div
+        style={{
+          marginBottom: index < RESUME.length - 1 ? SINGLE_SPACE * 2 : 0,
+        }}
+      >
+        <Line>
+          <Company>{job.company}</Company>
+          <BottomWrapper>
+            <Location>{job.location}</Location>
+          </BottomWrapper>
+        </Line>
+        <Line>
+          <Title>{job.title}</Title>
+          <DateRange>{job.dateRange}</DateRange>
+        </Line>
+        <Accomplishments>
+          {job.accomplishments.map((accomplishment) => (
+            <Accomplishment>{accomplishment}</Accomplishment>
+          ))}
+        </Accomplishments>
+      </div>
     ))}
-  </div>
+  </Container>
 );
